6575bb211309b46dca506646bc8474cd245fa263
Aravind Pradeep <aravind.pradeep.0802@gmail.com>
2024-12-05 10:55:11 +0530

    second commit

--- SevenRMart/src/test/java/testscript/Base.java
+++ SevenRMart/src/test/java/testscript/Base.java
@@ -10,41 +10,38 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
-public class Base 
-{
-	public WebDriver driver;
-	@BeforeMethod(alwaysRun=true)
-	@Parameters("browser")
-	public void initialiseBrowser(String browser) throws Exception
-	{
-		if(browser.equalsIgnoreCase("Chrome"))
-		{
-			driver =new ChromeDriver();
-		}
-		else if (browser.equalsIgnoreCase("Edge"))
-		{
-			driver = new EdgeDriver();
-		}
-		else if (browser.equalsIgnoreCase("FireFox"))
-			
-		{
-			driver = new FirefoxDriver();
-		}
-		
-		else 
-		{
-			throw new Exception("Invalid Browser");
-		}
-		driver=new ChromeDriver();
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.manage().window().maximize();
-	}
-	@AfterMethod(alwaysRun=true)
-	public void driverQuit()
-	{
-		driver.quit();
-	}
+public class Base {
+    public WebDriver driver;
 
-	
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("browser")
+    public void initialiseBrowser(String browser) throws Exception {
+    
+        if (browser.equalsIgnoreCase("Chrome")) {
+            
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            
+            driver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+           
+            driver = new FirefoxDriver();
+        } else {
+            throw new Exception("Invalid Browser: " + browser);
+        }
+
+        
+        driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+
+      
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void driverQuit() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
